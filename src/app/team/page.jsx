98c2e473da2074d70bb8d0c188e242762e3ddb67
Olu Sampson <remsams29@yahoo.com>
2025-06-13 04:33:59 +0100

    fix: Resolve 'Module not found' error for Section component

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -2,7 +2,7 @@
 import Cta from "@/app/ui/Cta";
 import Div from "@/app/ui/Div";
 import PageHeading from "@/app/ui/PageHeading";
-import Section from '../ui/Section';
+import SectionHeading from '@/app/ui/SectionHeading';
 import Team from '../ui/Team';
 import Spacing from '../ui/Spacing';
 import { teamData } from '../data/team';
@@ -17,9 +17,9 @@ export default function TeamPage() {
       />
       <Spacing lg="145" md="80" />
       <Div className="container">
-        <Section
-          heading="Our Team"
-          subHeading="Meet our team of professional."
+        <SectionHeading
+          title="Our Team"
+          subtitle="Meet our team of professional."
           variant="cs-style1 text-center"
         />
         <Spacing lg="90" md="45" />
